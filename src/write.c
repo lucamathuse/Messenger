e078de89f131ba94ffc8dab0754364ffb1e7efb5
Luca Mathuse <lucamathuse@Lucas-Air.fritz.box>
2019-03-10 18:52:52 +0100

    Added timestamps

--- src/write.c
+++ src/write.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 void write(char message[128]) {
   FILE *fptr;
+  time_t rawtime;
+  struct tm *info;
+  char buffer[80];
+
+  time( &rawtime );
+
+  info = localtime( &rawtime );
+
+  strftime(buffer,80,"%x - %I:%M:%p", info);
 
   fptr = fopen("./chat.txt", "a");
-  fprintf(fptr,"%s\n", message);
+  fprintf(fptr,"%s: %s\n", buffer,message);
   fclose(fptr);
 }
